e7ff141d2e1b2c7c2b20f91a6d5157c254b8df0a
rafael <rafael_mf3@hotmail.com>
2019-08-20 23:47:11 -0300

    profileScreen listing starred

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,19 @@
 import React, {useState, useEffect} from 'react';
-import { Text } from 'react-native';
 import api from '../../services/api';
 
-import { Container, Header, Avatar, Name, Bio } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author
+} from './styles';
 
 export default function User({ navigation }) {
   const [stars, setStars] = useState([]);
@@ -24,6 +35,19 @@ export default function User({ navigation }) {
         <Name>{user.name}</Name>
         <Bio>{user.bio}</Bio>
       </Header>
+      <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
     </Container>
   );
 }
